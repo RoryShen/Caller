e931206cac6c5896438e10fa033133f6c6e3fd5f
Rory <RoryShen@gmail.com>
2017-11-13 09:53:46 +0800

    first commit

---
+++ app/src/main/java/com/ck_telecom/caller/AnswerActivity.java
@@ -0,0 +1,26 @@
+package com.ck_telecom.caller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Rory on 2017/11/08   .
+ */
+
+public class AnswerActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/ck_telecom/caller/MainActivity.java
@@ -0,0 +1,27 @@
+package com.ck_telecom.caller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void answer(View view) {
+        Intent intent = new Intent(this, AnswerActivity.class);
+        startActivityForResult(intent, 1);
+        finish();
+    }
+
+    public void Call(View view) {
+        Intent CallIntent = new Intent(this, CallActivity.class);
+        startActivityForResult(CallIntent, 0);
+        finish();
+    }
+}
