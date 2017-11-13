568b932ae71e5317180fc5a687333c659d00c4aa
Rory <RoryShen@gmail.com>
2017-11-13 12:57:06 +0800

    add call function

--- app/src/main/java/com/ck_telecom/caller/CallActivity.java
+++ app/src/main/java/com/ck_telecom/caller/CallActivity.java
@@ -1,9 +1,19 @@
 package com.ck_telecom.caller;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.EditText;
+
+import static com.ck_telecom.caller.R.id.et_phone_ref_num;
 
 /**
  * Created by Rory on 2017/11/08   .
@@ -22,5 +32,26 @@ public class CallActivity extends Activity {
         Intent mainIntent = new Intent(this, MainActivity.class);
         startActivity(mainIntent);
         finish();
+    }
+
+    private void getUserInput() {
+
+
+        EditText phone = findViewById(et_phone_ref_num);
+        String phonenum = phone.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phonenum));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int i = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+            if (i != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 0);
+            }
+        }
+        startActivity(intent);
+
+
+    }
+
+    public void Call(View view) {
+        getUserInput();
     }
 }
